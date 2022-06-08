63149df493ff504894446d7975d9c945a9fc0680
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-06-08 18:40:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleTestes
+{
+    public class Program
+    {        
+        public static void Main(string[] args)
+        {
+            //IniciarSeletor();
+        }
+
+        public void IniciarSeletor()
+        {
+            string[] nomeVar = new string[4];            
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("Insira uma fruta ");
+                }
+                else
+                {
+                    Console.WriteLine("Insira outra fruta ");
+                }
+
+                nomeVar[i] = Console.ReadLine();
+                Console.WriteLine("A fruta " + (i + 1) + " é " + nomeVar[i]);
+                Console.Clear();
+            }
+            Console.WriteLine("As frutas inseridas foram " + nomeVar);
+        }
+    }
+                    
+}
